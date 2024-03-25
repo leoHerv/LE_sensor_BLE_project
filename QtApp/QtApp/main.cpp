37a77685f10b1ed527e877caffe8a252a385a325
Léo Hervouet <leoohervouet@gmail.com>
2024-03-25 17:23:35 +0100

    To add a qss style file to the app

--- QtApp/QtApp/main.cpp
+++ QtApp/QtApp/main.cpp
@@ -5,6 +5,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // Set the app style sheet
+    QFile styleSheetFile("./Toolery.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet (styleSheet);
+
+
     MainWindow w;
     w.show();
     w.resize(900, 600);
